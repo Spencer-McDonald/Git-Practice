c7433837ac7e648de8aca6d62aaab572e6ed68a0
Spencer <spencer@mcdonald.dev>
2020-10-14 12:17:58 -0500

    Added basic logging function to practice.py Ensured there are changes I would like to make in the future.

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
 #Practice python file for alteration with Git.
+import os
+import datetime
+
+today = datetime.datetime.now()
+def write_to_log(event):
+    log_event = event + " " + "[" + str(today) + "]\n"
+    with open("log.txt","a") as appender:
+        appender.write(log_event)
+
+write_to_log("log test")
