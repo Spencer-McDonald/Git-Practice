5402953ec98576d43e7140c12596f4e192df97b5
Spencer <spencer@mcdonald.dev>
2020-10-14 15:29:36 -0500

    "Accidentally" deleting a core part of the fuction (writer)

--- practice.py
+++ practice.py
@@ -7,7 +7,6 @@ today = datetime.datetime.now()
 # Function to take passed variable and log with date as an event.
 def write_to_log(event):
     log_event = "[" + str(today) + "]" + event + "\n"
-    with open("log.txt","a") as appender:
-        appender.write(log_event)
+
 
 write_to_log("log test")
