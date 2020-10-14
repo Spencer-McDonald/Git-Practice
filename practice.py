9f102f836bd5702eea84a31b8ffd0c9be12e3257
Spencer <spencer@mcdonald.dev>
2020-10-14 15:18:30 -0500

    Changed the order of the date and event message in the write_to_log function. Added documentation for the purpose of the function.

--- practice.py
+++ practice.py
@@ -3,8 +3,10 @@ import os
 import datetime
 
 today = datetime.datetime.now()
+
+# Function to take passed variable and log with date as an event.
 def write_to_log(event):
-    log_event = event + " " + "[" + str(today) + "]\n"
+    log_event = "[" + str(today) + "]" + event + "\n"
     with open("log.txt","a") as appender:
         appender.write(log_event)
 
